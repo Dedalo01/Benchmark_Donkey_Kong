116f4be52a6c1f054da0e5fcbc629667c9b39dc5
Gaetano <gaem195@gmail.com>
2023-11-16 14:14:17 +0100

    implement circular answer bar - need improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // FUNCTIONS
 
+// testing, decommenta dopo finito
 function hideShow(div1, div2) {
-  div1.classList.add("hide");
-  div2.classList.remove("hide");
+  /*   div1.classList.add("hide");
+  div2.classList.remove("hide"); */
 }
 
 function loadQuestion() {
@@ -71,6 +72,9 @@ function showResult() {
   const rightPercent = document.createElement("p"); //lui mi serve nell'if è il terzo p dentro al div di correct
   const wrongPercent = document.createElement("p");
   rightPercent.innerHTML = `${rightPercentage}&percnt;`;
+  // added gae
+  generateAnswerCircleProgressBar(parseInt(wrongToFix));
+  // end added gae
   wrongPercent.innerHTML = `${wrongPercentage}&percnt;`;
   correctDiv.appendChild(rightPercent);
   wrongDiv.appendChild(wrongPercent);
@@ -105,8 +109,9 @@ function showCongratulations() {
 
 function setTimer() {
   if (timerCount > 0) {
+    timerAnimation();
     timerCount--;
-    timerDiv.innerText = timerCount;
+    //timerDiv.innerText = timerCount;
   }
   console.log(timerCount);
 
@@ -117,6 +122,29 @@ function setTimer() {
     clearInterval(timer);
     selectAnswer(shuffledOptions1);
   }
+}
+
+// Timer animation
+function timerAnimation() {
+  timerNumber.innerText = timerCount;
+}
+
+// CORRECT ANSW CIRCLE
+function generateAnswerCircleProgressBar(wrongAnsw) {
+  let countPercent = 0;
+
+  let progress = setInterval(() => {
+    countPercent++;
+
+    valueContainer.textContent = `${countPercent}%`;
+    progressBar.style.background = `conic-gradient(#C2128D ${
+      countPercent * 3.6
+    }deg, #00FFFF ${countPercent * 3.6}deg)`;
+
+    if (countPercent == wrongAnsw) {
+      clearInterval(progress);
+    }
+  }, SPEED_ANSWER_BAR);
 }
 
 // COSTANT AND VARIABLES
@@ -195,14 +223,26 @@ let wrongAnswers = 0;
 let timer;
 const TOTAL_SECONDS_TIMER = 30;
 const SPEED_TIMER = 1000; //ms
+const SPEED_ANSWER_BAR = 30;
 let timerCount = TOTAL_SECONDS_TIMER;
 const timerDiv = document.querySelector("#timer");
+
+//animation timer
+const timerNumber = document.querySelector("#timerNumber");
+const timerSvg = document.querySelector("#timerSvg");
+console.log(timerSvg);
 
 //DOM selection
 const firstPage = document.querySelector("section");
 const secondPage = document.querySelector("#quizContainer");
 const thirdPage = document.querySelector("#resultsContainer");
 const fourthPage = document.querySelector("#rateUs");
+
+// CIRCULAR ANSWER
+let progressBar = document.querySelector("#answerProgressBar");
+let valueContainer = document.querySelector(
+  "#answerProgressBar .valueContainer"
+);
 
 // Btn selection
 const proceedButton = document.querySelector("#proceedBtn");
@@ -218,46 +258,3 @@ proceedButton.addEventListener("click", function () {
 rateUsButton.addEventListener("click", function () {
   hideShow(thirdPage, fourthPage);
 });
-
-// ROBA STELLE
-/* let maxStars = 10; // Numero massimo di stelle
-let currentRating = 0; */
-
-/* function generateStars() {
-  let starRatingDiv = document.getElementById("stars");
-
-  for (let i = 0; i < maxStars; i++) {
-    const div = document.createElement("div");
-
-    div.innerHTML = `<svg width="47" height="46" viewBox="0 0 47 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z" fill="#00FFFF"/>
-    </svg>`;
-    // aggiungere dinamicamente classe e nel css definire il fill scuro
-    // per svg
-
-    div.addEventListener("click", () => {
-      // ciclo for che prende da i
-    });
-    starRatingDiv.appendChild(div);
-  }
-}
-
-function changeStarColor() {
-  const stars = document.querySelectorAll("#stars svg");
-  // ciclo
-  // i
-  // da 0 a i --> rimuovi classe opaca
-  console.log(stars);
-}
-
-function rateStar(rating) {
-  currentRating = rating;
-  updateRatingDisplay();
-}
-
-function updateRatingDisplay() {
-  document.getElementById("ratingValue").innerText = "Rating: " + currentRating;
-}
-
-generateStars();
- */
